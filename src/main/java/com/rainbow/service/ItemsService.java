86577e6157272e109537ed5eed6d13b42e2644f6
chencaihong <836908728@qq.com>
2016-07-04 22:54:59 +0800

    商品修改的itemsmapper与service

--- src/main/java/com/rainbow/service/ItemsService.java
+++ src/main/java/com/rainbow/service/ItemsService.java
@@ -10,5 +10,12 @@ import java.util.List;
  * 一事专注，便是动人；一生坚守，便是深邃！
  */
 public interface ItemsService {
+    //查询商品列表
     public List<ItemsCustomer> findItemsList(ItemsQueryVo itemsQueryVo)throws Exception;
+    //根据ID查询商品信息
+    public ItemsCustomer findById(Integer id)throws Exception;
+    //修改商品信息
+    public void updateItems(Integer id,ItemsCustomer itemsCustomer)throws Exception;
+
+
 }
--- src/main/java/com/rainbow/service/impl/ItemsServiceImpl.java
+++ src/main/java/com/rainbow/service/impl/ItemsServiceImpl.java
@@ -1,9 +1,12 @@
 package com.rainbow.service.impl;
 
+import com.rainbow.entity.Items;
 import com.rainbow.entity.ItemsCustomer;
 import com.rainbow.entity.ItemsQueryVo;
+import com.rainbow.mapper.ItemsMapper;
 import com.rainbow.mapper.ItemsMapperCustomer;
 import com.rainbow.service.ItemsService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -16,8 +19,31 @@ public class ItemsServiceImpl implements ItemsService {
     //通过注入的方式注入mapper
     @Autowired
     private ItemsMapperCustomer itemsMapperCustomer;
+    @Autowired
+    private ItemsMapper itemsMapper;
     public List<ItemsCustomer> findItemsList(ItemsQueryVo itemsQueryVo) throws Exception {
 
         return itemsMapperCustomer.findItemsList(itemsQueryVo);
+    }
+
+    public ItemsCustomer findById(Integer id) throws Exception {
+        Items items=itemsMapper.selectByPrimaryKey(id);
+
+        ItemsCustomer itemsCustomer=new ItemsCustomer();
+        //讲items的属性值拷贝到itemsCustomer
+        BeanUtils.copyProperties(items,itemsCustomer);
+
+        return itemsCustomer;
+    }
+
+    public void updateItems(Integer id, ItemsCustomer itemsCustomer) throws Exception {
+//添加业务校验
+        //校验ID是否为空,如果为空则抛出异常,后面完善
+
+        //更新商品信息,updateByPrimaryKeyWithBLOBs根据ID更新表中的所有字段，包括大字段，
+        //注意：调用updateByPrimaryKeyWithBLOBs方法必须传入一个ID
+        itemsCustomer.setId(id);
+        itemsMapper.updateByPrimaryKeyWithBLOBs(itemsCustomer);
+
     }
 }
